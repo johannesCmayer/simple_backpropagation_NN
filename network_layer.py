107d5ae881e261deeb55b6a686c9a93c0487688b
Johannes C. Mayer <j.c.mayer240@gmail.com>
2018-10-17 20:40:26 +0200

    start work on model logic

--- network_layer.py
+++ network_layer.py
@@ -59,7 +59,7 @@ class FullyConnectedNL():
             loss += self.regularization(self.weights) / self.regularization_coef
         return loss
 
-    def loss_vrt(self, outputs, one_hot_targets):
+    def loss_vect(self, outputs, one_hot_targets):
         cost = 0
         for i, t in zip(outputs, one_hot_targets):
             cost += self.loss_func(i, t)
@@ -77,7 +77,7 @@ class FullyConnectedNL():
         self._nugde_at_flattend_index(arr, idx, -dx)
         return nudged_out
 
-    def gradient_vrt(self, grad_type, inputs, one_hot_targets, dx=0.01, batch_size=None):
+    def gradient_vect(self, grad_type, inputs, one_hot_targets, dx=0.01, batch_size=None):
         gradient_types = {'weights': self.weights,
                           'biasses': self.biasses}
         arr_to_calculate_grad = gradient_types.get(grad_type)
@@ -90,7 +90,7 @@ class FullyConnectedNL():
         for i in range(np.size(arr_to_calculate_grad)):
             nudged_out = self._eval_flattedend_nudged(arr_to_calculate_grad, inputs, i, dx)
             out = self.batch_eval(inputs)
-            gradient.append(-1 * (self.loss_vrt(nudged_out, one_hot_targets) - self.loss_vrt(out, one_hot_targets)) / dx)
+            gradient.append(-1 * (self.loss_vect(nudged_out, one_hot_targets) - self.loss_vect(out, one_hot_targets)) / dx)
         return np.reshape(gradient, weight_mat_shape)
 
     def eval_performance(self, inputs, one_hot_targets):
@@ -103,19 +103,19 @@ class FullyConnectedNL():
         start_time = time.time()
         prev_loss = None
         print('{}% Classified correctly bevore training'.format(self.eval_performance(inputs, one_hot_targets)))
-        current_loss = self.loss_vrt(self.batch_eval(inputs), one_hot_targets)
+        current_loss = self.loss_vect(self.batch_eval(inputs), one_hot_targets)
         print('loss at begining:', current_loss)
         try:
             for ep in range(epochs):
-                weight_gradient = self.gradient_vrt('weights', inputs, one_hot_targets, batch_size=batch_size)
-                biasses_gradient = self.gradient_vrt('biasses', inputs, one_hot_targets, batch_size=batch_size)
+                weight_gradient = self.gradient_vect('weights', inputs, one_hot_targets, batch_size=batch_size)
+                biasses_gradient = self.gradient_vect('biasses', inputs, one_hot_targets, batch_size=batch_size)
                 self.weights = self.weights + (weight_gradient * learning_rate)
                 self.biasses = self.biasses + (biasses_gradient * learning_rate)
                 print('\rBatch {}/{}'.format(ep, epochs), end='')
                 if verbose:
                     print()
                     print('gradient max val:', np.max(weight_gradient * learning_rate))
-                    current_loss = self.loss_vrt(self.batch_eval(inputs), one_hot_targets)
+                    current_loss = self.loss_vect(self.batch_eval(inputs), one_hot_targets)
                     print('loss:', current_loss)
                     if prev_loss:
                         print('loss improvment:', prev_loss - current_loss)
@@ -134,18 +134,40 @@ class FullyConnectedNL():
 
 
 class Model:
-    def __init__(self):
+    def __init__(self, loss_func=mse_loss):
         self.layers = []
+        self.loss = loss_func
 
     def add_layer(self, layer):
         self.layers.append(layer)
 
-    def eval(self):
+    def eval(self, inputs):
+        result = inputs
         for l in self.layers:
-            l.eval
+            result = l.eval(result)
+        return result
 
-    def backpropagate(self):
+    def batch_eval(self, inputs):
+        return [self.eval(i) for i in inputs]
+
+    def loss(self, inputs, labels):
+        return self.loss(inputs, labels)
+
+    def get_full_eval_graph(self, inputs):
+        result = inputs
+        layer_results = []
+        for l in self.layers:
+            result = l.eval(result)
+            layer_results.append(layer_results)
+        return layer_results
+
+    def backpropagate(self, inputs, labels):
+        eval_graph = self.get_full_eval_graph()
+        loss = self.loss(eval_graph[-1], labels)
+
+
         gradient = []
+
         return gradient
 
     def train(self):
